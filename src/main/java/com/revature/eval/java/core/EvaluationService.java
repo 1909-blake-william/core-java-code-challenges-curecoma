be822435a02cd222954dca630f86c51619f446fc
takumi <ctc.cheatz@gmail.com>
2019-10-14 15:58:41 -0400

    Assignment Completed

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -218,9 +218,8 @@ public class EvaluationService {
 
 	/**
 	 * 7. Implement a binary search algorithm.
-	 */
-
-	/**
+	 *
+	 *
 	 * Searching a sorted collection is a common task. A dictionary is a sorted list
 	 * of word definitions. Given a word, one can find its definition. A telephone
 	 * book is a sorted list of people's names, addresses, and telephone numbers.
